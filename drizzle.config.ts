8718203bf237041188e527be25a07748347bc6c8
Saket Kothari <saketkothari722@gmail.com>
2024-06-02 10:15:33 +0530

    ➕ drizzle orm & neon db setup

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local' });
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DRIZZLE_DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
