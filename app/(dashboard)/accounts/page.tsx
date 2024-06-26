013aebf4fd7af5feea75cd571138719fa49ac181
Saket Kothari <saketkothari722@gmail.com>
2024-06-26 22:20:57 +0530

    creating data-table component

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,23 @@
 'use client';
+
+import { Plus } from 'lucide-react';
+
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
 
 import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/data-table';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Plus } from 'lucide-react';
+
+import { Payment, columns } from './columns';
+
+const data: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+];
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
@@ -18,6 +32,9 @@ const AccountsPage = () => {
             Add new
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
       </Card>
     </div>
   );
