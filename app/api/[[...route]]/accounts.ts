e931660bffd94d5cd8538e65c0b4b4f0b98e377e
Saket Kothari <saketkothari722@gmail.com>
2024-06-27 19:33:45 +0530

    creating edit GET endpoint

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -29,6 +29,43 @@ const app = new Hono()
 
     return c.json({ data });
   })
+  .get(
+    '/:id',
+    zValidator(
+      'param',
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid('param');
+
+      if (!id) {
+        return c.json({ error: 'Bad Request: Missing id' }, 400);
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      const [data] = await db
+        .select({
+          // this accounts variable is the schema
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)));
+
+      if (!data) {
+        return c.json({ error: 'Not found' }, 404);
+      }
+
+      return c.json({ data });
+    }
+  )
   .post(
     '/',
     clerkMiddleware(),
