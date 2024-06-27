c31fa2650700b7fd90a9c96ef7f63e4bb4b16c10
Saket Kothari <saketkothari722@gmail.com>
2024-06-27 14:13:38 +0530

    removing the mock data & loading the actual data from our API

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -2,31 +2,19 @@
 
 import { Plus } from 'lucide-react';
 
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
 
 import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/data-table';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
-import { Payment, columns } from './columns';
-
-const data: Payment[] = [
-  {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '728ed52d',
-    amount: 50,
-    status: 'success',
-    email: 'ad@example.com',
-  },
-];
+import { columns } from './columns';
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data ?? [];
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -41,7 +29,7 @@ const AccountsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
+            data={accounts}
             filterKey="email"
             onDelete={() => {}}
             disabled={false} // will change it later
