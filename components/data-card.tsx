c075a7200137901eee593f97151366f1c4ebc585
Saket Kothari <saketkothari722@gmail.com>
2024-07-05 10:20:50 +0530

    added data in the cards and a skeleton when data is loading

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -1,7 +1,9 @@
 import { IconType } from 'react-icons';
 import { VariantProps, cva } from 'class-variance-authority';
 
-import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
+import { cn, formatCurrency, formatPercentage } from '@/lib/utils';
+import { CountUp } from '@/components/count-up';
 import {
   Card,
   CardContent,
@@ -71,6 +73,46 @@ export const DataCard = ({
           <Icon className={cn(iconVariant({ variant }))} />
         </div>
       </CardHeader>
+
+      <CardContent>
+        <h1 className="font-bold text-2xl mb-2 line-clamp-1 break-all">
+          <CountUp
+            preserveValue
+            start={0}
+            end={value}
+            decimals={2}
+            decimalPlaces={2}
+            formattingFn={formatCurrency}
+          />
+        </h1>
+        <p
+          className={cn(
+            'text-muted-foreground text-sm line-clamp-1',
+            percentageChange > 0 && 'text-emerald-500',
+            percentageChange < 0 && 'text-rose-500'
+          )}
+        >
+          {formatPercentage(percentageChange)} from last period
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm h-[192px]">
+      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+        <div className="space-y-2">
+          <Skeleton className="h-6 w-24" />
+          <Skeleton className="h-4 w-40" />
+        </div>
+        <Skeleton className="size-12" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="shrink-0 h-10 w-24 mb-2" />
+        <Skeleton className="shrink-0 h-4 w-40" />
+      </CardContent>
     </Card>
   );
 };
--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,22 +1,31 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
 import { FaPiggyBank } from 'react-icons/fa';
-
-import { useGetSummary } from '@/features/summary/api/use-get-summary';
+import { useSearchParams } from 'next/navigation';
+import { FaArrowTrendUp, FaArrowTrendDown } from 'react-icons/fa6';
 
 import { formatDateRange } from '@/lib/utils';
-
-import { DataCard } from '@/components/data-card';
+import { DataCard, DataCardLoading } from '@/components/data-card';
+import { useGetSummary } from '@/features/summary/api/use-get-summary';
 
 export const DataGrid = () => {
-  const { data } = useGetSummary();
+  const { data, isLoading } = useGetSummary();
 
   const params = useSearchParams();
   const to = params.get('to') || undefined;
   const from = params.get('from') || undefined;
 
   const dateRangeLabel = formatDateRange({ to, from });
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+        <DataCardLoading />
+        <DataCardLoading />
+        <DataCardLoading />
+      </div>
+    );
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
@@ -25,6 +34,22 @@ export const DataGrid = () => {
         value={data?.remainingAmount}
         percentageChange={data?.remainingChange}
         icon={FaPiggyBank}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Income"
+        value={data?.incomeAmount}
+        percentageChange={data?.incomeChange}
+        icon={FaArrowTrendUp}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Expenses"
+        value={data?.expensesAmount}
+        percentageChange={data?.expensesChange}
+        icon={FaArrowTrendDown}
         variant="default"
         dateRange={dateRangeLabel}
       />
