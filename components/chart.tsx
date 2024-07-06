8b168ccf8580ffb8170b159127899498675d7e88
Saket Kothari <saketkothari722@gmail.com>
2024-07-06 18:22:50 +0530

    building charts on the dashboard

---
+++ components/chart.tsx
@@ -0,0 +1,36 @@
+import { FileSearch } from 'lucide-react';
+
+import { AreaVariant } from '@/components/area-variant';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+type Props = {
+  data?: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
+export const Chart = ({ data = [] }: Props) => {
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <CardTitle className="text-xl line-clamp-1">Transactions</CardTitle>
+        {/* TODO: Add Select */}
+      </CardHeader>
+
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-full">
+            <FileSearch className="size-6 text-muted-foreground" />
+            <p className="text-muted-foreground text-sm">
+              No data for this period
+            </p>
+          </div>
+        ) : (
+          <AreaVariant data={data} />
+        )}
+      </CardContent>
+    </Card>
+  );
+};
