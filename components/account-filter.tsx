9bd0f70061b6bd47fe270c1fda602d2845f82915
Saket Kothari <saketkothari722@gmail.com>
2024-07-07 14:27:34 +0530

    📅 Add date filter functionality

--- components/account-filter.tsx
+++ components/account-filter.tsx
@@ -3,6 +3,7 @@
 import qs from 'query-string';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
+import { useGetSummary } from '@/features/summary/api/use-get-summary';
 import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
 
 import {
@@ -22,6 +23,7 @@ export const AccountFilter = () => {
   const from = params.get('from') || '';
   const to = params.get('to') || '';
 
+  const { isLoading: isLoadingSummary } = useGetSummary();
   const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
 
   const onChange = (newValue: string) => {
@@ -50,7 +52,7 @@ export const AccountFilter = () => {
     <Select
       value={accountId}
       onValueChange={onChange}
-      disabled={isLoadingAccounts}
+      disabled={isLoadingAccounts || isLoadingSummary}
     >
       <SelectTrigger className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 trasnsition">
         <SelectValue placeholder="Select account" />
